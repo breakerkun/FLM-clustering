b43cb8da42821f1a18fd51b21fef3fee4b2b449e
breakerkun <ziyiliu1125@163.com>
2021-08-04 22:35:45 +0800

    0804

--- R_functions.R
+++ R_functions.R
@@ -5,23 +5,15 @@ Beta89 <- function(t) {
   beta89.t <- rep(0,length(t))
   for(i in 1:length(t))
   {
-    beta89.t[i] = 2-10*t[i]
+    beta89.t[i] = 2-6*t[i]
   }
   return(beta89.t)
-}
-Betaquad <- function(t) {
-  betaquad.t <- rep(0,length(t))
-  for(i in 1:length(t))
-  {
-    betaquad.t[i] = -8-20*t[i]*t[i]+10*t[i];
-  }
-  return(betaquad.t)
 }
 Betacubic <- function(t) {
   betacubic.t <- rep(0,length(t))
   for(i in 1:length(t))
   {
-    betacubic.t[i] = -6+10*t[i]*t[i]-10*t[i]+6*(t[i])^3;
+    betacubic.t[i] = -3+10*t[i]*t[i]-10*t[i]+6*(t[i])^3;
   }
   return(betacubic.t)
 }
@@ -29,7 +21,7 @@ Betaexp <- function(t) {
   betaexp.t <- rep(0,length(t))
   for(i in 1:length(t))
   {
-    betaexp.t[i] = 2-exp(2*t[i])+3*sin(2*t[i]);
+    betaexp.t[i] = 1.5-exp(2*t[i])+3*sin(2*t[i]);
   }
   return(betaexp.t)
 }
@@ -68,64 +60,6 @@ InnerProd <- function(Betaf,basisfd,j) {
   flog <- function(t) {Betaf(t)*BFun(t)}
   in.prod <- integrate(flog,a,b) 
   return(in.prod$value)
-}
-
-# generate simulation data
-DataYBeta <- function(n,n1,n2,n3,n4, rangeval, Beta.i, Beta.ii,Beta.iii,Beta.iv,aMat_Fun){
-  #m and d of X(t)
-  nnknots <- 15
-  nnorder <- 5
-  nnbasis <- nnknots + nnorder - 2
-  basisfdData <- create.bspline.basis(rangeval=rangeval, nbasis=nnbasis, norder=nnorder)
-  
-  #inner product of <beta#_j, B_j>
-  G1 <- matrix(0,nrow=nnbasis, ncol=1)
-  for (j in 1:nnbasis){
-    G1[j,1] <- InnerProd(Beta.i,basisfdData,j)
-  } 
-  G2 <- matrix(0,nrow=nnbasis, ncol=1)
-  for (j in 1:nnbasis){
-    G2[j,1] <- InnerProd(Beta.ii,basisfdData,j)
-  } 
-  G3 <- matrix(0,nrow=nnbasis, ncol=1)
-  for (j in 1:nnbasis){
-    G3[j,1] <- InnerProd(Beta.iii,basisfdData,j)
-  } 
-  G4 <- matrix(0,nrow=nnbasis, ncol=1)
-  for (j in 1:nnbasis){
-    G4[j,1] <- InnerProd(Beta.iv,basisfdData,j)
-  } 
-  # aij, coefficient of basis functionB_j
-  if (aMat_Fun == "Norm") 
-  {
-    aMat <- matrix(rnorm(n1*nnbasis,mean=2,sd=1),n1,nnbasis)
-    aMat2 <- matrix(rnorm(n2*nnbasis,mean=2,sd=1),n2,nnbasis)
-    aMat3 <- matrix(rnorm(n3*nnbasis,mean=2,sd=1),n3,nnbasis)
-    aMat4 <- matrix(rnorm(n4*nnbasis,mean=2,sd=1),n4,nnbasis)
-  }else if (aMat_Fun=="Unif")
-  {
-    aMat <- matrix(runif(n1*nnbasis,min=-2,max=2),n1,nnbasis)
-    aMat2 <- matrix(runif(n2*nnbasis,min=-2,max=2),n2,nnbasis)
-    aMat3 <- matrix(runif(n3*nnbasis,min=-2,max=2),n3,nnbasis)
-    aMat4 <- matrix(runif(n4*nnbasis,min=-2,max=2),n4,nnbasis)
-  }
-  
-  y1Tru <- aMat%*%G1
-  y2Tru <- aMat2%*%G2
-  y3Tru <- aMat3%*%G3
-  y4Tru <- aMat4%*%G4
-  
-  y1 <- y1Tru + rnorm(n1,mean=0,sd=1)
-  y2 <- y2Tru + rnorm(n2,mean=0,sd=1)
-  y3 <- y3Tru + rnorm(n3,mean=0,sd=1)
-  y4 <- y4Tru + rnorm(n4,mean=0,sd=1)
-  
-  ycol = rbind(y1,y2,y3,y4)
-  
-  a<- rbind(aMat,aMat2,aMat3,aMat4)
-  x<- fd(coef=t(a), basisobj=basisfdData)
-  data <- list(x=x,ycol=ycol,y1=y1,y2=y2,y1Tru=y1Tru,y2Tru=y2Tru)
-  return(data)
 }
 
 knots_eq3 <- function(x, k, m){
